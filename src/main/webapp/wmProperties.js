e4444b3a6f55fc079823c061c02f635af2d4e4c3
root <root@ff617b86f380>
2017-05-22 11:04:33 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "PassportAPP",
+  "homePage" : "Main",
+  "name" : "PassportAPP",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
