7b9f772aec38cfdf751814ffbd6f1633e49c132e
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2017-05-30 05:57:10 +0000

    updated source from prod env New files added - [CourtCaseProceedings,passportapp_addrs] Files modified - [Main,pages-config.json] Removed files - [ReissueofPassport,FreshPassport]

---
+++ src/main/webapp/WEB-INF/prefabs/passportapp_addrs/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "passportapp_addrs",
+  "homePage" : "Main",
+  "name" : "passportapp_addrs",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.07"
+};
